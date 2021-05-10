08573d2c8f7f33fc67a22052c5f9bee2e0713df4
Rafael Corsi <rafael.corsi@insper.edu.br>
2021-05-10 14:04:54 -0300

    nova versao codigo organizado

--- src/aps2/aps2.c
+++ src/aps2/aps2.c
@@ -1,25 +1,29 @@
 /*
- * config.c
+ * aps2.c
  *
- * Created: 07/05/2021 22:51:32
+ * Created: 10/05/2021 11:11:07
  *  Author: RafaelCF4
  */ 
 
 #include "aps2.h"
 
-// extern volatile int ecgCnt = 0;
-// 
-// void TC0_Handler(void) {
-//   volatile uint32_t ul_dummy;
-// 
-//   ul_dummy = tc_get_status(TC0, 0);
-//   
-//   int ecgSize = sizeof(ecg)/sizeof(ecg[0]);
-//   if (ecgCnt >= ecgSize) {  ecgCnt=0; }
-//   else { ecgCnt++; }
-//   
-//   dacc_write_conversion_data(DACC_BASE, ecg[ecgCnt], DACC_CHANNEL);
-// }
+void TC0_Handler(void) {
+  volatile uint32_t ul_dummy;
+
+  ul_dummy = tc_get_status(TC0, 0);
+
+  if (g_ecgCnt >= g_ecgSize) {
+    g_ecgCnt = 0;
+    g_ecgDelayCnt = g_ecgDelayValue;
+  }
+
+  if (g_ecgDelayCnt > 0) {
+    g_ecgDelayCnt--;
+    } else {
+    g_ecgCnt++;
+  }
+  dacc_write_conversion_data(DACC_BASE, ecg[g_ecgCnt], DACC_CHANNEL);
+}
 
 static void config_DAC(void) {
   /* Enable clock for DACC */
@@ -55,3 +59,36 @@ void TC_init_modificado(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
 
   tc_start(TC, TC_CHANNEL);
 }
+
+void vTimerEcgCallback( TimerHandle_t xTimer ) {
+  // delay entre ECGs de [200, 500]
+  g_ecgDelayValue = rand() % (500 + 1 - 200) + 200;
+  printf("%d \n", g_ecgDelayValue);
+}
+
+void task_aps2(void *pvParameters) {
+  
+  g_ecgDelayValue = 200;
+  config_DAC();
+  TC_init_modificado(TC0, ID_TC0, 0, DAC_FS);
+  xTimer = xTimerCreate("ECG", pdMS_TO_TICKS(ECG_CHANGE_TIME), pdTRUE, (void *) 0, vTimerEcgCallback);
+  xTimerStart( xTimer, 0 );
+  
+  // oxiometria
+  char ox;
+  float theta = 0;
+
+  for (;;)  {
+    
+    // Oxiometria: sin entre 88 e 100
+    if (theta > 2*PI) {
+      theta = 0;
+    }
+    ox = (char) ((arm_sin_f32(theta) + 1.0)*12.0/2.0 + 88.0);
+    theta = theta + 0.3;
+    xQueueSend(xQueueOx, &ox, 100);
+
+    vTaskDelay(2000);
+  }
+}
+
